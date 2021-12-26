44572bd9b98639cad8ccf9629ecb3f6a69059c16
Josh <josh@gdovindesigns.com>
2021-12-26 13:44:15 -0500

    demo complete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,7 @@
-import { Fancybox } from "@fancyapps/ui/src/Fancybox/Fancybox.js";
+import {Carousel} from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox.css";
+
+
+const myCarousel = new Carousel(document.querySelector(".carousel"), {
+    // Options
+  })
